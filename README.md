be3a58a057709ba91de95affe25e135105160301
Ian Spence <ian@ecn.io>
2019-03-19 17:29:08 -0700

    Add linux (deb and rpm) support

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Media Player
 
-A simple media player for macOS and Windows (and maybe Linux?)
+A simple media player for macOS, Windows, and Linux (Debian and RPM)
+
+## Development Requirements
+
+Media Player is designed to be developed on a *nix compatible machine. Do not attempt to develop with Cygwin.
+
+For macOS, the following packages are required **to release the application**:
+
+- wine
+- rpm
+- dpkg
+- fakeroot
--- release.js
+++ release.js
@@ -4,6 +4,7 @@ exec('rm -rf build/');
 exec('mkdir -p build/artifacts');
 exec('rm -rf static/');
 exec('rm -rf node_modules/');
+exec('rm -rf copy/');
 exec('./install.sh');
 exec('npm install');
 exec('gulp');
@@ -11,9 +12,12 @@ exec('gulp release');
 
 const windowsInstaller = require('electron-winstaller');
 const macInstaller = require('electron-installer-dmg');
+const redhatInstaller = require('electron-installer-redhat');
+const debianInstaller = require('electron-installer-debian');
 const package = require('./package.json');
 
 function packageApp(platform) {
+    console.info('Packaging application for ' + platform);
     let packager = require('electron-packager');
     return packager({
         dir: '.',
@@ -29,6 +33,7 @@ function packageApp(platform) {
         osxSign: false,
         asar: true,
         darwinDarkModeSupport: true,
+        executableName: 'media-player'
     });
 }
 
@@ -56,14 +61,75 @@ function buildWindows() {
             title: 'Media Player',
             iconUrl: 'https://raw.githubusercontent.com/ecnepsnai/Media-Player/master/img/icon.ico',
             setupIcon: 'img/icon.ico',
-            exe: 'Media Player.exe',
+            exe: 'media-player.exe',
             setupExe: 'Media-Player-' + package.version + '.exe',
+        }).then(() => {
+            exec('rm build/artifacts/media-player-' + package.version + '-full.nupkg');
+            exec('rm build/artifacts/RELEASES');
         });
     });
 }
 
-buildDarwin().then(() => {
-    buildWindows().then(() => {
-        console.log('Finished!');
+function buildLinux() {
+    var packagePromise = packageApp('linux');
+    return packagePromise.then(() => {
+        var redhatPromise = new Promise((resolve, reject) => {
+            redhatInstaller({
+                src: 'build/Media Player-linux-x64/',
+                name: 'Media Player',
+                dest: 'build/artifacts',
+                icon: 'img/icon.png',
+                categories: [
+                    'Video',
+                ],
+                arch: 'x86_64'
+            }, ((err) => {
+                if (err !== null) {
+                    console.error('Error packaging app for redhat: ', err);
+                    reject(err);
+                    return;
+                }
+                try {
+                    exec('mv "build/artifacts/Media-Player-' + package.version + '.x86_64.rpm" "build/artifacts/Media-Player-' + package.version + '.rpm"');
+                } catch (e) {
+                    reject(e);
+                    return;
+                }
+                resolve();
+            }));
+        });
+        var debianPromise = new Promise((resolve, reject) => {
+            debianInstaller({
+                src: 'build/Media Player-linux-x64/',
+                name: 'Media Player',
+                dest: 'build/artifacts',
+                icon: 'img/icon.png',
+                categories: [
+                    'Video',
+                ],
+                arch: 'x86_64'
+            }).then(() => {
+                try {
+                    exec('mv "build/artifacts/media-player_' + package.version + '_x86_64.deb" "build/artifacts/Media-Player-' + package.version + '.deb"');
+                } catch (e) {
+                    reject(e);
+                    return;
+                }
+                resolve();
+            }, (err) => {
+                console.error('Error packaging app for debian: ', err);
+                reject(err);
+                return;
+            });
+        });
+        return Promise.all([redhatPromise, debianPromise]);
     });
-});
+}
+
+async function run() {
+    await buildDarwin();
+    await buildWindows();
+    await buildLinux();
+}
+run();
+
