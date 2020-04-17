8d8d76a14cb795d1da8dd58c7e24c7d47abf31a2
Ian Spence <ian@ecnepsnai.com>
2020-04-16 21:35:55 -0700

    Update everything

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sS https://use.fontawesome.com/releases/v5.7.2/fontawesome-free-5.7.2-web.zip > fontawesome.zip
+curl -sS https://use.fontawesome.com/releases/v5.13.0/fontawesome-free-5.13.0-web.zip > fontawesome.zip
 unzip fontawesome.zip >/dev/null
 rm fontawesome.zip
 mkdir -p copy/assets/css
